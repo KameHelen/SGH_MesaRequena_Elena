6408a4b1a852d1700578c59bd1887bafed0bd79f
KameHelen <elemesreq@gmail.com>
2025-10-09 19:34:33 +0200

    creando clases

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ $pass = '';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$optiones = [
+$opciones = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     
